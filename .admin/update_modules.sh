0c052bf0d8b23932587056de6c56721e00c638b7
Anthony Vadala <git@anthonyvadala.me>
2019-10-21 12:52:13 -0400

    Update update_modules.sh

--- .admin/update_modules.sh
+++ .admin/update_modules.sh
@@ -45,7 +45,7 @@ for section in csplit-sections-* ; do
 				continue
 			fi
 			if [[ "$name" == "(Module Name)" ]] ; then continue; fi
-			url=$(grep 'https://git' $module | cut -d[ -f 2 | cut -d] -f 1)
+			url=$(grep 'https://git' $module | grep -v 'module.json' | head -n 1 | cut -d[ -f 2 | cut -d] -f 1)
 			# Get first line of the description
 			description=$(cat $module | grep -A 1 '### Description' | tail -n 1)
 			echo "Found module : $name"
@@ -57,7 +57,7 @@ for section in csplit-sections-* ; do
 					echo "   URL : $url"
 					dir=$(basename "$url")
 					if [[ -d "$section_name/$dir" ]] ; then
-						git submodule update --remote "$section_name/$dir"
+						git submodule update --remote --init "$section_name/$dir"
 					else
 						git submodule add $url "$section_name/$dir"
 					fi
